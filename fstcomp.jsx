ff168cfbca15a773607c37fb271d5b91473fa775
SahirMalkani <41510686+SahirMalkani@users.noreply.github.com>
2024-01-03 21:29:19 +0500

    Add files via upload

---
+++ fstcomp.jsx
@@ -0,0 +1,33 @@
+
+import nnn from "../assets/img/bg-masthead.jpg"
+import { Butt } from "./butt";
+
+const Fstcomp0=()=>{
+
+   
+    return (<>
+    
+<div className="container" style={{marginTop:"1%",height:"400px", backgroundPosition:"center",  width: "1400px" , backgroundImage: `url(${nnn})`}}>          
+
+
+
+
+<div className="col-md-12"  style={{display:"flex"}} >
+<h6>Genetate more leads with professional <br /> approach this is made by sahri</h6>
+</div>
+
+
+<div style={{display:"flex"}}>
+ <input  type="text" /> 
+ <Butt naam="save23" />
+</div>
+
+
+</div>
+
+    </>
+    );
+    };
+    
+    
+    export {Fstcomp0};
